4f716d0a6b88e8f7118659914960276d7ea22f63
bayramov92 <bayramov92@gmail.com>
2016-11-01 09:49:18 +0200

    Initial Commit

---
+++ testGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGithub
+//
+//  Created by Medzhit Bayramov on 0111.16 г..
+//  Copyright © 2016 г. Bulbera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
